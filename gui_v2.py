dbb5ab661eebc432771ce0fa11ade74887ffef92
Louis Cundari III <louiscundari3@outlook.com>
2023-01-09 00:35:02 -0500

    Implemented Oil Analysis page - OilAnalysisPage class

--- gui_v2.py
+++ gui_v2.py
@@ -57,7 +57,7 @@ class MainMenuPage(tk.Frame):
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
-        container = tk.LabelFrame(self, text='Page Container: Main', relief='ridge')
+        container = tk.LabelFrame(self, text='Page: Main Menu', relief='ridge')
         container.grid(row=0, column=0, sticky='nsew')
         
         container.grid_rowconfigure(0, weight=1)
@@ -81,7 +81,7 @@ class MainMenuPage(tk.Frame):
         btn2 = tk.Button(container, text='Page 2', command=lambda: parent.switch_frame(SectorAnalysisPage))
         btn2.grid(row=1, column=0, sticky='nsew')
         
-        btn3 = tk.Button(container, text='Page 3', command=lambda: parent.switch_frame(CoastDownPage))
+        btn3 = tk.Button(container, text='Page 3', command=lambda: parent.switch_frame(CoastdownPage))
         btn3.grid(row=2, column=0, sticky='nsew')
         
         btn4 = tk.Button(container, text='Page 4', command=lambda: parent.switch_frame(OilAnalysisPage))
@@ -94,7 +94,7 @@ class SessionAnalysisPage(tk.Frame):
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
-        container = tk.LabelFrame(self, text='Page Container: Session', relief='ridge')
+        container = tk.LabelFrame(self, text='Page: Session Analysis', relief='ridge')
         container.grid(row=0, column=0, sticky='nsew')
         
         container.grid_rowconfigure(0, weight=1)
@@ -180,7 +180,7 @@ class SectorAnalysisPage(tk.Frame):
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
-        container = tk.LabelFrame(self, text='Page Container: Session', relief='ridge')
+        container = tk.LabelFrame(self, text='Page: Sector Analysis', relief='ridge')
         container.grid(row=0, column=0, sticky='nsew')
         
         container.grid_rowconfigure(0, weight=1)
@@ -268,14 +268,14 @@ class SectorAnalysisPage(tk.Frame):
         
         # stat4_label = Label(info_frame, text='Text ', wraplength=350, justify=LEFT)
         # stat4_label.place(y=135, x=10)
-class CoastDownPage(tk.Frame):
+class CoastdownPage(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
 
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
-        container = tk.LabelFrame(self, text='Page Container: Session', relief='ridge')
+        container = tk.LabelFrame(self, text='Page: Coastdown Analysis', relief='ridge')
         container.grid(row=0, column=0, sticky='nsew')
         
         container.grid_rowconfigure(0, weight=1)
@@ -339,9 +339,85 @@ class CoastDownPage(tk.Frame):
 class OilAnalysisPage(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
-        tk.Label(self, text='This is Page 2').pack(side='top', fill='x', pady=10)
-        tk.Button(self, text='Return to start page',
-                  command=lambda: parent.switch_frame(MainMenuPage)).pack()
+        
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        container = tk.LabelFrame(self, text='Page: Oil Analysis', relief='ridge')
+        container.grid(row=0, column=0, sticky='nsew')
+        
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_rowconfigure(1, weight=3)
+        container.grid_rowconfigure(2, weight=3)
+        container.grid_rowconfigure(3, weight=3)
+
+        container.grid_columnconfigure(0, weight=2)
+        container.grid_columnconfigure(1, weight=2)
+        container.grid_columnconfigure(2, weight=1)
+
+        ## Page layout
+        header_frame = tk.LabelFrame(container, bd=1, relief='flat')
+        treeview1_frame = tk.LabelFrame(container, text='100% Oil', font=14, bd=2, relief='ridge')
+        treeview2_frame = tk.LabelFrame(container, text='x% Oil', font=14, bd=2, relief='ridge')
+        plot1_frame = tk.LabelFrame(container, text='Coolant Temp vs Time', font=14, bd=2, relief='ridge')
+        plot2_frame = tk.LabelFrame(container, text='Coolant Temp vs RPM', font=14, bd=2, relief='ridge')
+        plot3_frame = tk.LabelFrame(container, text='RPM % Change', font=14, bd=2, relief='ridge')
+        button_frame = tk.LabelFrame(container, text='Options', font=14, bd=2, relief='ridge')
+        info_frame = tk.LabelFrame(container, text='Info', font=14, bd=2, relief='ridge')
+
+        header_frame.grid(row=0, column=0, columnspan=3, sticky='nsew', padx=2, pady=2)
+        treeview1_frame.grid(row=1, rowspan=2, column=0, sticky='nsew', padx=2, pady=2)
+        treeview2_frame.grid(row=1, rowspan=2, column=1, sticky='nsew', padx=2, pady=2)
+        plot1_frame.grid(row=1, rowspan=1, column=2, sticky='nsew', padx=2, pady=2)
+        plot2_frame.grid(row=2, rowspan=1, column=2, sticky='nsew', padx=2, pady=2)
+        plot3_frame.grid(row=3, rowspan=1, column=2, sticky='nsew', padx=2, pady=2)
+        button_frame.grid(row=3, column=0, sticky='nsew', padx=2, pady=2)
+        info_frame.grid(row=3, column=1, columnspan=1, sticky='nsew', padx=2, pady=2)
+        
+        # Widgets
+        page_title = tk.Label(header_frame, text='UConn FSAE DAQ Multitool')
+        page_title.place(relx=.5, rely=.5, anchor=tk.CENTER)
+        page_title.config(font=('arial', 14))
+        
+        main_btm = MainMenuButton(header_frame)
+
+        ## Treeviews
+        tree1 = TreeViewWidget(treeview1_frame)
+        tree2 = TreeViewWidget(treeview2_frame)
+        
+        ## Main Buttons
+        button1 = tk.Button(button_frame, text='Oil File 1', command=lambda: select_datafile1(tree1_data, filepath_label1))
+        button1.place(y=30, relx=.25, width=80, anchor=tk.CENTER)
+
+        button2 = tk.Button(button_frame, text='Oil File 2', command=lambda: select_datafile2(tree2_data, filepath_label2))
+        button2.place(y=70, relx=.25, width=80, anchor=tk.CENTER)
+
+        button3 = tk.Button(button_frame, text='Clear Data', command=lambda: clear_treeview([tree1_data, tree2_data], [filepath_label1, filepath_label2]))
+        button3.place(y=30, relx=.75, width=80, anchor=tk.CENTER)
+
+        button4 = tk.Button(button_frame, text='Process Data', command=lambda: init_oil_analysis([df_data1, df_data2], int(app.spinbox_max_temp_diff_from_avg.get()))) # ** displays all graphs at once.
+        button4.place(y=70, relx=.75, width=80, anchor=tk.CENTER)
+
+        spinbox1 = tk.Spinbox(button_frame, from_=0, to=99, textvariable=app.spinbox_max_temp_diff_from_avg)
+        spinbox1.place(y=140, relx=.25, width=80, anchor=tk.CENTER)
+        spinbox1_label = tk.Label(button_frame, text='Max temperature difference from\naverage (used to remove outliers)')
+        spinbox1_label.place(relx=.25, y=110, anchor=tk.CENTER)
+
+        ## Statistics
+        filepath_label1 = tk.Label(info_frame, text='File 1: ', wraplength=450, justify=tk.LEFT)
+        filepath_label1.place(y=10, x=10)
+
+        filepath_label2 = tk.Label(info_frame, text='File 2: ', wraplength=450, justify=tk.LEFT)
+        filepath_label2.place(y=55, x=10)
+
+        stat1_label = tk.Label(info_frame, text=' ', wraplength=350, justify=tk.LEFT)
+        stat1_label.place(y=75, x=10)
+
+        stat2_label = tk.Label(info_frame, text='Mandatory columns:', wraplength=350, justify=tk.LEFT)
+        stat2_label.place(y=95, x=10)
+
+        stat3_label = tk.Label(info_frame, text='Time, Distance, S8_RPM, S*eot, S8_ect1, S8_eop', wraplength=350, justify=tk.LEFT)
+        stat3_label.place(y=115, x=10)
 
 
 class MainMenuButton():
